455be839b8dec547fbbc4c13c5a42d0ee20a0ef6
Vantch <ui@vantch.com>
2019-10-29 16:43:58 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var apps = new Vue({
+    el: '#app',
+    // methods: {
+    // 	logMessage() {
+    // 		console.log(this.message)
+    // 	}
+    // },
+    data: {
+        message: 'Hello',
+        title: 'hello',
+        welcome: true,
+    }
+})
